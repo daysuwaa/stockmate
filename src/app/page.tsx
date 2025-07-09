d46afa9557a8d6a88d4aea4ffc7435b62d28f236
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-07-09 09:47:59 +0100

    Introduced new components: Footer, Navbar, Hero, Features, Testimonials, UseCase for the Home page. - Created login and signup pages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Footer from "./components/Footer";
+import Features from "./components/Home/Features";
+import Hero from "./components/Home/Hero";
+import Testimonials from "./components/Home/Testimonials";
+import UseCase from "./components/Home/UseCase";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <div>
-      <h1>hii</h1>
+      <Navbar/>
+      <Hero/>
+      <Features/>
+      <UseCase/>
+      <Testimonials/>
+      <Footer/>
+
      
       
     </div>
