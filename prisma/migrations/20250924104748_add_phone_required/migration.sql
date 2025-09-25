08392168486ffe870143c4437fc016f095da13ce
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-09-25 11:27:01 +0100

    feat: prisma migrations

---
+++ prisma/migrations/20250924104748_add_phone_required/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "website" TEXT,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "email", "id", "name", "passwordHash", "phone", "updatedAt", "website") SELECT "createdAt", "email", "id", "name", "passwordHash", "phone", "updatedAt", "website" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
