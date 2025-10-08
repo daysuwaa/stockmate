fb16cb35ef2d82848b9d6e9c52b7f711c003db58
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-10-08 08:50:15 +0100

    fix: separate client providers from layout to resolve Next.js metadata export error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,17 @@
-"use client";
-
 import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "react-hot-toast";
-import { store } from "./redux/store/store";
-import { Provider } from "react-redux";
-import ThemeWrapper from "./settings/ThemeProvider"; 
 import "./globals.css";
-// import AuthInitializer from "./AuthInitializer";
-import AuthPersist from "./AuthPersists";
+import ClientProviders from "./ClientProviders";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-
 export const metadata = {
   title: "StockMate | Track products, monitor stock, and manage your inventory from one clean dashboard",
-  description:
-    "Track products, monitor stock, and manage your inventory from one clean dashboard ",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  description: "Track products, monitor stock, and manage your inventory from one clean dashboard",
+  icons: { icon: "/favicon.ico" },
   openGraph: {
     title: "Stock Mate | Track products, monitor stock, and manage your inventory from one clean dashboard",
-  description:
-    "Track products, monitor stock, and manage your inventory from one clean dashboard ",
+    description: "Track products, monitor stock, and manage your inventory from one clean dashboard",
     url: "https://stockmate-five.vercel.app/",
     type: "website",
     images: [
@@ -36,21 +24,14 @@ export const metadata = {
     ],
   },
 };
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        
-        <Provider store={store}>
-          <AuthPersist/>
-          <ThemeWrapper>
-            {/* <AuthInitializer /> */}
-          
-            {children}
-          </ThemeWrapper>
-          <Toaster position="top-right" reverseOrder={false} />
-        </Provider>
+        <ClientProviders>
+        {children}
+        </ClientProviders>
       </body>
     </html>
   );
