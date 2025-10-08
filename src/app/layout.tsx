884acab1822a5e090ac24e85f2ba7e6f3426af92
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-10-08 08:42:05 +0100

    refactoring the image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,30 @@ import AuthPersist from "./AuthPersists";
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
+
+export const metadata = {
+  title: "StockMate | Track products, monitor stock, and manage your inventory from one clean dashboard",
+  description:
+    "Track products, monitor stock, and manage your inventory from one clean dashboard ",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Stock Mate | Track products, monitor stock, and manage your inventory from one clean dashboard",
+  description:
+    "Track products, monitor stock, and manage your inventory from one clean dashboard ",
+    url: "https://stockmate-five.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://stockmate-five.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "StockMate",
+      },
+    ],
+  },
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
