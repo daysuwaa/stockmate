9463f8b600b7c7835428b1032c54f4093b6f6b98
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-10-28 17:51:59 +0100

    change  folder name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Footer from "../app/components/layout/Footer";
-import Features from "../app/components/home/Features";
-import Hero from "../app/components/home/Hero";
-import Testimonials from "../app/components/home/Testimonials";
-import UseCase from "../app/components/home/UseCase";
+import Features from "./components/index/Features";
+import Hero from "./components/index/Hero";
+import Testimonials from "./components/index/Testimonials";
+import UseCase from "./components/index/UseCase";
 import Navbar from "../app/components/layout/Navbar";
 
 export default function Home() {
